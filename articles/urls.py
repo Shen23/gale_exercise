a78348c1ec632956f75e1d3aa356778570df50bd
FJ <shen_fujun@hotmail.com>
2015-04-10 01:06:52 -0400

    pictures working

--- articles/urls.py
+++ articles/urls.py
@@ -1,11 +1,14 @@
-from django.conf.urls import include, url
+from django.conf.urls import patterns, include, url
 from articles import views
+from django.conf.urls.static import static
+from django.conf import settings
 
 
-urlpatterns = [
+urlpatterns = patterns(
+	'',
 	url(r'^$', views.ArticleListView.as_view(), name="article"),
 
 
     url(r'^articles/(?P<pk>\d+)$', views.ArticleDetail.as_view(), name="article_detail"),
-    ]
+    )+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
--- gale_exe/urls.py
+++ gale_exe/urls.py
@@ -1,16 +1,19 @@
-from django.conf.urls import include, url
+from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from articles import views
 from articles.models import Article
+from django.conf.urls.static import static
+from django.conf import settings
 
-
-urlpatterns = [
+urlpatterns = patterns(
 #url('^$', 'articles.views.home', name = 'home'),
     #url('^$', views.ArticleListView.as_view(model = Article)),
+    '',
     url(r'^admin/', include(admin.site.urls)),
     #url(r'^markdown/', include('django_markdown.urls')),
     url(r'^', include('articles.urls')),
-]
+)+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
     # Examples:
     # url(r'^$', 'gale_exe.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
